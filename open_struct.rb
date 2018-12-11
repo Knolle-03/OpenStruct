bfb0f46d71b497eae415e17c66cd1d19200b3fe2
Knolle-03 <Knolle-03@users.noreply.github.com>
2018-12-11 16:27:08 +0100

    Up and running(!) Not quite sure how since don't understand how instance_variable_set operates buuuuut it works!

--- open_struct.rb
+++ open_struct.rb
@@ -4,42 +4,27 @@ PERSON = 'Person'
 
 persons = []
 
-file = File.open('persons', 'r')
+file = File.open('persons.txt', 'r')
+i = 0
 loop do
   person_attr = {}
   file.each do |line|
-    break if line == '###'
+    break if line.strip! == '###'
     next unless line.include?(':')
 
     attr = line.split(':')
     attr.each(&:strip!)
-
+    person_attr[attr[0].gsub(/[ ]/, '_').to_sym] = attr[1]
   end
+  persons << instance_variable_set("@Person_#{i += 1}", OpenStruct.new(person_attr))
+  break if file.eof?
+
 end
 
 
 
 
-
-person = OpenStruct.new
-person.name = 'Peter'
+puts persons[2].class
 
 
 
-
-#
-#def get_linear_hash(filename)
-#  unless File.file?(filename)
-#    return nil
-#  end
-#  file = File.open(filename)
-#  hash = {}
-#  file.each do|line|
-#    if line.match? /[0-9]/
-#      words = line.split(' ')
-#      hash[words[0]] = words[1].to_f
-#    end
-#  end
-#  file.close
-#  hash
-#end
