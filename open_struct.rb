579c4f2accb0a3d18b734ac40d077dc3475775f8
Knolle-03 <Knolle-03@users.noreply.github.com>
2018-12-12 10:51:24 +0100

    shorter and better readability(?)

--- open_struct.rb
+++ open_struct.rb
@@ -1,30 +1,18 @@
 require 'ostruct'
 
-PERSON = 'Person'
-
 persons = []
-
 file = File.open('persons.txt', 'r')
-i = 0
 loop do
   person_attr = {}
   file.each do |line|
-    break if line.strip! == '###'
+    break if line.strip == '###'
     next unless line.include?(':')
 
     attr = line.split(':')
     attr.each(&:strip!)
-    person_attr[attr[0].gsub(/[ ]/, '_').to_sym] = attr[1]
+    person_attr[attr[0].gsub(/[ ]/, '_')] = attr[1]
   end
-  persons << instance_variable_set("@Person_#{i += 1}", OpenStruct.new(person_attr))
+  persons << OpenStruct.new(person_attr)
   break if file.eof?
-
 end
-
-
-
-
-puts persons[2].class
-
-
-
+puts persons
