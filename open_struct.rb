6701c56c551bce7a6e7a658f004ac95137fed69e
Knolle-03 <Knolle-03@users.noreply.github.com>
2018-12-13 01:30:03 +0100

    Up and running(!) Not quite sure how since don't understand how instance_variable_set operates buuuuut it works!

--- open_struct.rb
+++ open_struct.rb
@@ -1,54 +1,45 @@
+# Aufgabe a09
+# Team ChillyCrabs | NeamTame
+# Author:: Lennart Draeger
+# Author:: Robert Gnehr
+# 1.2
 require 'ostruct'
 require 'date'
 
-# 1.1
-
-person = OpenStruct.new
-class << person
-  def age_calc(day_of_birth)
-    now = Time.now.utc.to_date
-    now.year - day_of_birth.year - (now.month > day_of_birth.month || now.month == day_of_birth.month && now.day >= day_of_birth.day ? 0 : 1)
-  end
-end
-
-person.first_name = 'Lars'
-person.last_name = 'Peters'
-person.birthday = Date.new(1991, 12, 12)
-person.e_mail = 'lars_peters@gmail.com'
-person.age = person.age_calc(person.birthday)
-
-
-# 1.2
+persons_file = 'persons.txt'
+invalids_file = 'invalids.txt'
 musts = %w[first_name last_name]
+
 persons = []
 invalids = []
-file = File.open('persons.txt', 'r')
-loop do
-  person_attr = {}
-  file.each do |line|
-    break if line.strip == '###'
-    next unless line.include?(':')
 
-    attr = line.split(':')
-    attr.each(&:strip!)
-    person_attr[attr[0].gsub(/[ ]/, '_')] = attr[1]
+unless File.zero?(persons_file)
+  file = File.open(persons_file, 'r')
+  loop do
+    person_attr = {}
+    file.each do |line|
+      break if line.strip == '###'
+      next unless line.include?(':')
+
+      attr = line.split(':')
+      attr.each(&:strip!)
+      person_attr[attr[0].gsub(/[ ]/, '_')] = attr[1]
+    end
+    remains = musts - person_attr.keys
+    remains.empty? ? persons << OpenStruct.new(person_attr) : invalids << OpenStruct.new(person_attr)
+    break if file.eof?
   end
-  remains = musts - person_attr.keys
-  remains.empty? ? persons << OpenStruct.new(person_attr) : invalids << OpenStruct.new(person_attr)
-  break if file.eof?
 end
 
-
-puts persons
-puts '---------------------'
 unless invalids.empty?
-  file = File.open('invalids.txt', 'a')
+  File.exist?(invalids_file) ? File.truncate(invalids_file, 0) : nil
+  file = File.open(invalids_file, 'a')
   file.write"You may want to rework the following #{invalids.length} entries.\n"
   invalids.each do |entry|
     entry.to_h.each_pair do |k, v|
-      file.write("#{k}: #{v}\n")
+      file.write"#{k}: #{v}\n"
     end
-    file.write("###\n")
+    file.write"###\n"
   end
 end
 
