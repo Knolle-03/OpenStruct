1a0d4f138d68717dff6dd547ab18ff89cabe13e2
RGnehr <r.gnehr@gmx.de>
2018-12-17 02:38:04 +0100

    Tidied up PersonStruct

--- person_struct.rb
+++ person_struct.rb
@@ -5,41 +5,50 @@
 
 require 'ostruct'
 
-# OpenStruct subclass for Persons with methods to return its name only or its
-# attributes only.
+# OpenStruct subclass for Persons with methods to return selected subsets of its
+# attributes.
 class PersonStruct < OpenStruct
 
   DEFAULT_NAME_IDENTIFIERS = %w[name firstname lastname middlename vorname nachname].freeze
+  DEFAULT_CONTACT_IDENTIFIERS = %w[tel phone telefon telephone fax email website].freeze
 
   def initialize(*args)
     super(*args)
     @name_identifiers = DEFAULT_NAME_IDENTIFIERS
+    @contact_identifiers = DEFAULT_CONTACT_IDENTIFIERS
+  end
+
+  def name
+    select_attributes(@name_identifiers, true)
+  end
+
+  def attributes
+    select_attributes(@name_identifiers, false)
+  end
+
+  def contact_information
+    select_attributes(@contact_identifiers, true)
   end
 
   def to_s
     return "#<#{self.class}> (emtpy)" if @table.empty?
 
-    name_string = ''
-    @table.each do |k, v|
-      next unless @name_identifiers.include?(k.to_s.downcase.delete('_'))
-
-      name_string << "#{' ' unless name_string.empty?}#{v}"
-    end
-
-    name_string.empty? ? "#<#{self.class}> (no names found)" : name_string
+    name = select_attributes(@name_identifiers, true).values.join(' ')
+    name.empty? ? "#<#{self.class}> (no names found)" : name
   end
 
-  def list_attributes
-    attributes = ''
-    @table.each do |k, v|
-      next if @name_identifiers.include?(k.to_s.downcase.delete('_'))
+  def list_all
+    return to_s unless @table.size > 2
 
-      attributes << "#{', ' unless attributes.empty?}#{k}: #{v}"
-    end
-    attributes
+    complete_string = to_s << ' ('
+    attributes.to_a.each { |pair| complete_string << pair.join(': ') + ', ' }
+    complete_string.delete_suffix!(', ') << ')'
   end
 
-  def list_with_name
-    "#{to_s} #{'(' + list_attributes + ')' if @table.size > 2 }"
+  private
+
+  def select_attributes(id_ary, from_ary)
+    choice = from_ary ? :select : :reject
+    @table.send(choice) { |key| id_ary.include?(key.to_s.downcase.delete('_')) }
   end
 end
